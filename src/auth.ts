2cbe86ad479703c7b6afb031552c82c6a682db86
youssefbelfeki <youssef.belfeki@gmail.com>
2025-05-26 14:17:24 +0100

    Google Provider

--- src/auth.ts
+++ src/auth.ts
@@ -5,12 +5,21 @@ import authConfig from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
-    async jwt({ token }) {
-      return token;
-    },
     async session({ session, token }) {
-      if (session.user && token.sub)  session.user.id = token.sub
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+        const user = await prisma.user.findUnique({ where: { id: token.sub}})
+        if(user) session.user.role = user.role
+      }
       return session;
+    },
+  },
+  events: {
+    async linkAccount({ user }) {
+      await prisma.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
     },
   },
   adapter: PrismaAdapter(prisma),
