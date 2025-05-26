dd3ad9924c979ea47705031e86c30b81b6584cae
youssefbelfeki <youssef.belfeki@gmail.com>
2025-05-26 13:25:27 +0100

    Github Provider

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,9 +5,10 @@ import React from "react";
 const ProfilePage = async () => {
   const session = await auth();
   return (
-    <div>
+    <div className="flex items-center justify-center flex-col">
       {session?.user && (
         <>
+        <p className="mb-5 text-center px-9">{JSON.stringify(session)}</p>
           <h1 className="text-3xl font-bold mb-7">
             Welcome {session.user.name} to your profile
           </h1>
